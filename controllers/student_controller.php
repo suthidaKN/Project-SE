6738dcb54165a4783ba979b71bd4345d38e2ae1d
suthidaKN <91516432+suthidaKN@users.noreply.github.com>
2022-03-25 02:49:23 +0700

    addcompany

--- controllers/student_controller.php
+++ controllers/student_controller.php
@@ -29,38 +29,6 @@
             require_once("./views/student/addCompany.php");
         }
         
-        public function addCompany(){
-            $CID = Company::CountCompanyAll();
-            settype($CID,"integer");
-            $CID = $CID+1;
-            
-            $CompanyName= $_POST['CompanyName']; 
-            $CAddress= $_POST['CAddress'];
-            $CStreet= $_POST['CStreet'];
-            $CTumbon= $_POST['id_amphure'];
-            $CType= $_POST['CType'];
-            $CPhone= $_POST['CPhone'];
-            $CFax= $_POST['CFax'];
-            $CManager= $_POST['CManager'];
-            $CMngPosition= $_POST['CMngPosition'];
-            $CCoordinator= $_POST['CCoordinator'];
-            $CCoorPosition= $_POST['CCoorPosition'];
-            $CCoorDepartment= $_POST['CCoorDepartment'];
-            $CCoorPhone= $_POST['CCoorPhone'];
-            $CCoorEmail= $_POST['CCoorEmail'];
-            $CRecivePostion= $_POST['CRecivePostion'];
-            $CJobDescription= $_POST['CJobDescription'];
-            $CNumber= $_POST['CNumber'];
-            $CSkillReq= $_POST['CSkillReq'];
-
-            echo $CID,$CompanyName, $CAddress,$CStreet, $CTumbon,$CType,$CPhone,$CFax,$CManager,$CMngPosition,$CCoordinator,$CCoorPosition,$CCoorDepartment,$CCoorPhone,$CCoorEmail,
-            $CRecivePostion,$CJobDescription,$CNumber,$CSkillReq;
-            Company::Add($CID,$CompanyName, $CAddress,$CStreet, $CTumbon,$CType,$CPhone,$CFax,$CManager,$CMngPosition,$CCoordinator,$CCoorPosition,$CCoorDepartment,$CCoorPhone,$CCoorEmail,
-            $CRecivePostion,$CJobDescription,$CNumber,$CSkillReq);
-            StudentController::newRequirement();
-            
-        
-        }
         public function checkStatus(){
             $account = Account::getID($_SESSION["user"]);
             $JobRequirment = JobRequirment::getJobReqStudent($account->stuID);
